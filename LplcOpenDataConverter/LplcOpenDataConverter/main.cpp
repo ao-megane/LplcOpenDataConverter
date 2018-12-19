cce32fec8ea674499d0bb72e87cbdd99903f8a5d
ao-megane <ao.megane.0716@gmail.com>
2018-12-20 02:32:32 +0900

    a

--- LplcOpenDataConverter/LplcOpenDataConverter/main.cpp
+++ LplcOpenDataConverter/LplcOpenDataConverter/main.cpp
@@ -369,9 +369,9 @@ int main(){
 		}
 		ofs << endl;
 	}*/
-	ofs << "2018/6•½“ú7`8‚Ì‡Œv" << endl;
+	ofs << "2018/6•½“ú7`8‚Ìˆê“ú‚ ‚½‚è‚Ì•½‹Ï" << endl;
 	for (int i = 0; i < 19; i++) {
-		ofs << i+1 << "," << sensor[i].GetnOut() + sensor[i].GetnIn() << endl;
+		ofs << i+1 << "," << (sensor[i].GetSumnOut() + sensor[i].GetSumnIn())/workdaynum << endl;
 	}
 	ofs.close();
 
